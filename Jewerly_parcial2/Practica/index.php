85698981aca1ffbe598153d59d8ad277ada119f8
Michellezta <101204440+Michellezta@users.noreply.github.com>
2022-11-05 18:46:04 -0700

    cambios 5

---
+++ Jewerly_parcial2/Practica/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jewelry</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<body>
+    <?php include 'menu.php' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbotron">
+                <h1 class="display-4">The shape of our love</h1>
+                    <p class="lead">Choose the ideal jewel for each personality.</p>
+                    </div>
+                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                <div class="carousel-inner" >
+                    <div class="carousel-item active">
+                    <img src="imagen.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="imagen 3.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagen 2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    
+                  </div>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center">
+        <hr>
+        2022 &copy; jeweler's
+    </footer>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
